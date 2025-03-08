3329127f3a4bd8216a3dd9f04a503c4b01a50087
unknown <mjanuszek@student.agh.edu.pl>
2025-03-08 18:55:23 +0100

    fixed

--- seminaria1/script.js
+++ seminaria1/script.js
@@ -1,37 +1,51 @@
 document.addEventListener("DOMContentLoaded",function(){
-  let flexDirections = document.querySelectorAll("#flex-direction button")
-  let div0 = document.querySelector(".flex-direction-demo")
-  flexDirections.forEach(button => {
-    button.addEventListener("click", function() {
-      div0.style.flexDirections = button.dataset.value
-    })
+  // flex direction
+  const flexDirectionButtons = document.querySelectorAll("#flex-direction button");
+  const div0 = document.querySelector(".flex-direction-demo");
+  
+  flexDirectionButtons.forEach(button => {
+    button.addEventListener("click", () => {      
+      div0.style.flexDirection = button.dataset.value;
+    });
   });
-  let flexWrap = document.querySelectorAll("#flex-wrap button")
-  let div1 = document.querySelector(".flex-wrap-demo")
-  flexWrap.forEach(button => {
-    button.addEventListener("click", function() {
-      div1.style.flexDirections = button.dataset.value
-    })
+
+  // flex wrap
+  const flexWrapButtons = document.querySelectorAll("#flex-wrap button");
+  const div1 = document.querySelector(".flex-wrap-demo");
+
+  flexWrapButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      div1.style.flexWrap = button.dataset.value;
+    });
   });
-  let justifyContent = document.querySelectorAll("#justify-content button")
-  let div2 = document.querySelector(".justify-content-demo")
-  justifyContent.forEach(button => {
-    button.addEventListener("click", function() {
-      div2.style.flexDirections = button.dataset.value
-    })
+
+  // justify content
+  const justifyContentButtons = document.querySelectorAll("#justify-content button");
+  const div2 = document.querySelector(".justify-content-demo");
+
+  justifyContentButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      div2.style.justifyContent = button.dataset.value;
+    });
   });
-  let alignItems = document.querySelectorAll("#align-items button")
-  let div3 = document.querySelector(".align-items-demo")
-  alignItems.forEach(button => {
-    button.addEventListener("click", function() {
-      div3.style.flexDirections = button.dataset.value
-    })
+
+  // align items
+  const alignItemsButtons = document.querySelectorAll("#align-items button");
+  const div3 = document.querySelector(".align-items-demo");
+  
+  alignItemsButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      div3.style.alignItems = button.dataset.value;
+    });
   });
-  let alignContent = document.querySelectorAll("#align-content button")
-  let div4 = document.querySelector(".align-content-demo")
-  alignContent.forEach(button => {
-    button.addEventListener("click", function() {
-      div4.style.flexDirections = button.dataset.value
+
+  // align content
+  const alignContentButtons = document.querySelectorAll("#align-content button");
+  const div4 = document.querySelector(".align-content-demo");
+  
+  alignContentButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      div4.style.alignContent = button.dataset.value;     
     })
   });
 })

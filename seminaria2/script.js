5f8b7ab5ad72671e9e4ebced666f42fa25e9ad4e
unknown <mjanuszek@student.agh.edu.pl>
2025-03-17 13:52:56 +0100

    comments

--- seminaria2/script.js
+++ seminaria2/script.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
-  function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
   // hide/show element
   $("#hide-me").on("click", function () {
     $(this).parent().parent().fadeToggle();
-    setTimeout(()=>{
-      $(".goback").fadeToggle().css("display","flex");
-    },1000)
+    setTimeout(() => {
+      $(".goback").fadeToggle().css("display", "flex");
+    }, 1000);
   });
 
   $("#go-back").on("click", function () {
     $(this).parent().fadeToggle();
-    setTimeout(()=>{
+    setTimeout(() => {
       $("#hide").fadeToggle();
-    },1000)
+    }, 1000);
   });
 
   // dynamic elements
   $("#create-elements").on("click", function () {
     const menu = $("#elements");
     menu.empty();
-    items = Array.from({ length: $('#amount').val() }, (_, i) => `Item ${i + 1}`);    
-    items.forEach((item) => {     
+    items = Array.from(
+      { length: $("#amount").val() },
+      (_, i) => `Item ${i + 1}`
+    );
+    items.forEach((item) => {
       const menuItem = $("<div>")
         .addClass("menu-item")
         .text(item)
@@ -54,7 +53,6 @@ $(document).ready(function () {
       const leftWidth = (relativeX / containerWidth) * 100;
       const rightWidth = 100 - leftWidth - 2;
 
-      $("#center").css("left", `${relativeX / containerWidth}px`);
       $("#left").css("width", `${leftWidth}%`);
       $("#right").css("width", `${rightWidth}%`);
     });
@@ -81,12 +79,11 @@ $(document).ready(function () {
             let last = path[path.length - 1];
             if (Math.abs(last[0] - i) <= 1 && Math.abs(last[1] - j) <= 1) {
               path.push([i, j]);
-              $(".lastselected").addClass("selected")
-              $(".lastselected").removeClass("lastselected")
+              $(".lastselected").addClass("selected");
+              $(".lastselected").removeClass("lastselected");
               $(this).addClass("lastselected");
             }
           }
-
         });
       $("#field").append(cell);
     }
@@ -107,7 +104,7 @@ $(document).ready(function () {
   });
 
   $("#move").on("click", function () {
-    if (!game){
+    if (!game) {
       alert("Start the game first!");
       return;
     }
@@ -127,32 +124,4 @@ $(document).ready(function () {
     $(".cell").removeClass("selected lastselected");
     $("#pawn").remove();
   });
-  $("#auto").on("click", function () {
-    if (path.length > 0) {
-      const startCell = path[0];
-      $(`#cell-${startCell[0]}-${startCell[1]}`).append(pawn);
-      game = true;
-      async function loopWithDelay(){
-        for(let i = 0; i < path.length - 1; i++){
-          await delay(200);
-          if (currentIndex < path.length - 1) {
-            currentIndex++;
-            const nextCell = path[currentIndex];
-            $(`#cell-${nextCell[0]}-${nextCell[1]}`).append(pawn);
-          
-          } 
-          await delay(500);
-        }
-        if(currentIndex == path.length - 1) {
-          alert("Pawn has reached the end of the path!");
-        }
-      }
-      loopWithDelay()
-    }
-    
-  });
-
 });
-
-
-
